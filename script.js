9b9da6e9133908dcaacad9bbca20d55c78ab2f6b
Juana Paiva <juana@ibm.com>
2023-09-24 13:53:22 -0300

    Versão inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Juana, usando brinco de estrla prateado, cabelo solto e fundo escuro."
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Juana sorrindo, usando cabelo solto e fundo claro."
+    )
+  }
+}
